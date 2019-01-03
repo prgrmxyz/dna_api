9531b07a0cb528991c7ec858098f475b84a578ec
Archit Dwivedi <architdwivedi00@gmail.com>
2019-01-03 21:25:17 +0530

    Added Bulk Insert

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -4,6 +4,9 @@ var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./8193.23andme.6530')
 });
 connection.getConnection().then(db => {
+    var col = db.collection('data');
+    var batch = col.initializeOrderedBulkOp();
+    var count = 1;
     lineReader.on('line',  (line) => {
         line = line.trim();
         if (line[0] !== '#' && line.length > 0) {
@@ -14,7 +17,14 @@ connection.getConnection().then(db => {
                 'position': data[2],
                 'genotype': data[3],
             };
-            db.collection("data").insertOne(json).then(value => console.log("inserted: " + data[0]));
+            console.log("Inserted: ", count);
+            count ++;
+            batch.insert(json);
+            // db.collection("data").insertOne(json).then(value => console.log("inserted: " + data[0]));
         }
+    }).on('close', function(line) {
+        batch.execute(function(err, result) {
+            console.log("BULK INSERTED COMPLETED");
+        });
     });
 });
