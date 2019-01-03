cb84021859beef2d8d7120a0f994b21610ce00b3
Archit Dwivedi <architdwivedi00@gmail.com>
2019-01-03 20:57:48 +0530

    Aggregeation Api Done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../utils/db');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (request, response, next) {
+    connection.getConnection().then(db => {
+        var output = db.collection("data").aggregate([
+            {
+                $match: {}
+            },
+            {
+                $group: {
+                    _id: "$chromosome",
+                    count: {$sum: 1}
+                }
+            }
+        ]).toArray();
+
+        output.then(value => {
+            response.status(200).json(value);
+        });
+    }, reason => {
+        throw reason
+    });
 });
 
 module.exports = router;
